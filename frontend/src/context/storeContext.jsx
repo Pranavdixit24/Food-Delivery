96eb9c033ef85af33affd13101b36423f0e8a1a0
Pranav <dixitpranav24@gmail.com>
2025-08-31 14:31:04 +0530

    commit

--- frontend/src/context/storeContext.jsx
+++ frontend/src/context/storeContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props)=>{
     const [cartItems,setCartItems]=useState({})
-    const url="http://localhost:4000"
+    // const url="http://localhost:4000"
+    const url = "https://food-delivery-qavd.onrender.com"
     const [token,setToken]=useState("")
     const addToCart=(itemId)=>{
         if(!cartItems[itemId]){
